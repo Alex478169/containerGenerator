de7ad54b877ada5df16f02e2f01aba419c51f89c
Alex478169 <alessandro.mor@outlook.it>
2025-12-09 16:58:00 +0000

    react project updated

--- react_containerGenerator/containerGenerator/src/App.tsx
+++ react_containerGenerator/containerGenerator/src/App.tsx
@@ -3,19 +3,28 @@ import './App.css'
 function App() {
  return (
     <>
-     <p>in questa pagina puoi creare un container compilando i campi qui presenti. Infatti, dovrai inserire il suo nome e l'immagine associata. Che cosa è l'immagine? L'immagine è come se fosse una copia statica della propria applicazione. 
-	  NB. Nella creazione di un container ci sono altri attributi importanti, ma io qui inserisco quelli basi </p>
-    <form action="/" method="post">
-        <input type="text" />
-        <input type="text" />
-        <input type="text" />
-        <input type="text" />
-        <input type="text" />
-        <input type="text" />
-        <input type="text" />
-    </form>
+    <div>
+     <form action="/" method="POST">
+			<div style={{display:"flex", flexDirection: "column"}}>
+				<h1 style={{textAlign: "center"}}>Container generator</h1>
+				<div style={{display: "flex", flexDirection: "row", justifyContent: "space-evenly"}}>
+					<div style={{display:"flex", flexDirection: "column",  marginBottom: "25px;"}}>
+						<p style={{fontSize: "large;"}}>container_name</p>
+						<p style={{fontSize: "large;"}}>porte</p>
+						<p style={{fontSize: "large"}}>immagine</p>
+					</div>
+					<div style={{ display:"flex", flexDirection: "column", justifyContent: "space-evenly", marginBottom: "25px" }}>
+						<input type="text" style={{borderRadius:"10px", fontSize: "medium"}} name="cname" placeholder="container name"/>
+						<input type="text" style={{borderRadius:"10px",fontSize: "medium"}} name="cimage" placeholder="container ports"/>			
+						<input type="text" style={{borderRadius:"10px", fontSize: "medium"}} name="cports" placeholder="container ports"/>
+					</div>
+				</div>
+				<button type="submit" style={{marginRight: "auto", marginLeft: "auto", fontSize: "medium", borderRadius: "10px", paddingLeft: "30px", paddingRight: "30px", paddingTop: "15px", paddingBottom: "15px"}}>crea il tuo container</button>
+			</div>
+		</form>
+    </div>
     </>
   )
 }
 
-export default App
+export default App;
